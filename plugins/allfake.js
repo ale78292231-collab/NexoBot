cd3f5a6b84c06ee4e7e0fdedcf4f922b4f30cf02
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-14 18:47:27 -0500

    Create allfake.js

---
+++ plugins/allfake.js
@@ -0,0 +1,120 @@
+import pkg from '@whiskeysockets/baileys'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
+
+var handler = m => m
+handler.all = async function (m) {
+
+global.getBuffer = async function getBuffer(url, options) {
+try {
+options ? options : {}
+var res = await axios({
+method: "get",
+url,
+headers: {
+'DNT': 1,
+'User-Agent': 'GoogleBot',
+'Upgrade-Insecure-Request': 1
+},
+...options,
+responseType: 'arraybuffer'
+})
+return res.data
+} catch (e) {
+console.log(`Error : ${e}`)
+}}
+  
+//creador y otros
+global.creador = 'Wa.me/50378630152'
+global.ofcbot = `${conn.user.jid.split('@')[0]}`
+global.asistencia = 'Wa.me/50378630152'
+global.namechannel = '⏤͟͞ू⃪𓏲𝐒𝐀𝐊𝐔𝐍𝐀𓍲̈͜𝐁𝐎𝐓̄𓊓̵̬Channel⋆͙̈么͟͞──'
+global.namegrupo = '⏤͟͞ू⃪  ̸̷𝐒𝐀𝐊𝐔𝐍𝐀 𝐁𝐎𝐓 𝐌𝐃☦︎ 𝐆𝐫𝐮𝐩𝐨𑁯ᰍ'
+global.namecomu = '𝐒𝐀𝐊𝐔𝐍𝐀'
+global.listo = '📌 *Aquí tienes ᕕ( ᐛ )ᕗ*'
+
+//Ids channel
+global.idchannel = '120363422395892347@newsletter'
+global.canalIdM = ["120363422395892347@newsletter", "120363422395892347@newsletter"]
+global.canalNombreM = ["⏤͟͞ू⃪  ̸𝐒𝐀𝐊𝐔𝐍𝐀 𝐁𝐎𝐓 ☦︎ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥𑁯ᰍ", "❄️.𝐒𝐀𝐊𝐔𝐍𝐀 𝐁𝐎𝐓 𝐂𝐇𝐀𝐍𝐍𝐄𝐋.𝐎𝐅𝐈𝐂𝐈𝐀𝐋͙̈么͟͞──"]
+global.channelRD = await getRandomChannel()
+
+global.d = new Date(new Date + 3600000)
+global.locale = 'es'
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
+global.mes = d.toLocaleDateString('es', {month: 'long'})
+global.año = d.toLocaleDateString('es', {year: 'numeric'})
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+
+//Reacciones De Comandos.!
+global.rwait = '🕐'
+global.done = '✅'
+global.error = '❌'
+
+//Emojis determinado de Sakuna Bot 
+global.emoji = '🩵'
+global.emoji2 = '💎'
+global.emoji3 = '🩵'
+global.emoji4 = '💎'
+global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
+
+
+//Enlaces
+var canal = 'https://whatsapp.com/channel/0029VbBhpX5A89MqEHNYTd2V'  
+let canal2 = 'https://whatsapp.com/channel/0029VbBhpX5A89MqEHNYTd2V'
+var git = 'https://github.com/ale78292231-collab' 
+var youtube = '' 
+var github = 'https://github.com/ale78292231-collab/NexoBot.git' 
+let correo = 'wazaponni@gmail.com'
+global.redes = [canal, git, github, correo].getRandom()
+
+let category = "imagen"
+const db = './src/database/db.json'
+const db_ = JSON.parse(fs.readFileSync(db))
+const random = Math.floor(Math.random() * db_.links[category].length)
+const randomlink = db_.links[category][random]
+const response = await fetch(randomlink)
+const rimg = await response.buffer()
+global.icons = rimg
+
+var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
+global.saludo = hour;
+
+global.nombre = m.pushName || 'Anónimo'
+global.taguser = '@' + m.sender.split("@s.whatsapp.net")
+var more = String.fromCharCode(8206)
+global.readMore = more.repeat(850)
+
+  
+global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
+
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
+}}, { quoted: m }
+
+global.icono = [
+'https://files.catbox.moe/2e0k1j.jpg',
+'https://files.catbox.moe/t62gbg.jpg',
+'https://files.catbox.moe/2e0k1j.jpg',
+'https://files.catbox.moe/t0ww2n.jpg',
+'https://files.catbox.moe/ai3r5w.jpg',
+'https://files.catbox.moe/86jkuc.jpg'
+].getRandom()
+
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+  }
+
+async function getRandomChannel() {
+let randomIndex = Math.floor(Math.random() * canalIdM.length)
+let id = canalIdM[randomIndex]
+let name = canalNombreM[randomIndex]
+return { id, name }
+}
