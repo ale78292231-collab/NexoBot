3bbfe935d6324597b0709afb47b36d5337cfceed
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-14 19:12:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import fs, { readdirSync, statSync, unlinkSync, existsSync, mkdirSync, readFileS
 import yargs from 'yargs';
 import { spawn, execSync } from 'child_process'
 import lodash from 'lodash'
-import { kanekiAIJadiBot } from './plugins/jadibot-serbot.js'
+import { blackJadiBot } from './plugins/jadibot-serbot.js'
 import chalk from 'chalk'
 import syntaxerror from 'syntax-error'
 import pino from 'pino'
